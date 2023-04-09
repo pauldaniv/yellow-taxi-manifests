1aa211e527710a5fdc5aa5e400eb25299ebf8b07
Pavlo Daniv <paulsegfault@gmail.com>
2023-04-09 20:15:59 +0300

    Add aws creds and helper script for state sync

---
+++ manage_state.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+ACTION=$1
+AVAILABLE_ACTIONS="Available actions: [prepare, apply, destroy]"
+
+cd "$(cd "$(dirname "$0")/.."; pwd)"
+
+if [[ -z "$ACTION" ]]; then
+  echo "Action not specified. $AVAILABLE_ACTIONS"
+  exit 1
+fi
+
+if [[ "$ACTION" = "prepare" ]]; then
+  echo "Preparing infrastructure..."
+  echo "Connecting to cluster"
+  aws eks --region us-east-2 update-kubeconfig --name yellow-taxi
+  echo "Adding extra repositories"
+  helm repo add nginx-ingress https://helm.nginx.com/stable
+  echo "Running helmfile init"
+  helmfile init
+elif [[ "$ACTION" = "apply" ]]; then
+  echo "Appling infrastructure..."
+  helmfile apply
+elif [[ "$ACTION" = "destroy" ]]; then
+  echo "Destroying infrastructure..."
+  helmfile destroy
+else
+  echo "Unknown action provided: $ACTION. $AVAILABLE_ACTIONS"
+fi
+
+echo "Done!"
