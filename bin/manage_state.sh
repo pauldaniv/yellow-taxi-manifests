4015dc2d06c620aae109211d077e8bec5a2aa9fb
Pavlo <paulsegfault@gmail.com>
2023-05-31 22:47:51 +0300

    Fix ENV-secrets

--- bin/manage_state.sh
+++ bin/manage_state.sh
@@ -25,7 +25,7 @@ function init() {
 function create() {
   echo "Applying CRDs..."
   helm upgrade --install -n kube-system secrets-provider-aws aws-secrets-manager/secrets-store-csi-driver-provider-aws
-  helm upgrade --install -n kube-system csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver
+  helm upgrade --install -n kube-system csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver --set syncSecret.enabled=true
   kubectl create namespace yellow-taxi
 #  kubectl create namespace external-secrets
 #  kubectl apply -f charts/secret/templates/spc.yaml -n yellow-taxi
