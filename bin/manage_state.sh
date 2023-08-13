6590fe08c95b7539fd4cf3bf00caba3bbf0c1e0c
Pavlo <paulsegfault@gmail.com>
2023-08-13 18:18:45 +0300

    Re-create

--- bin/manage_state.sh
+++ bin/manage_state.sh
@@ -30,7 +30,7 @@ function create() {
   kubectl create namespace yellow-taxi
   helm upgrade --install -n kube-system secrets-provider-aws aws-secrets-manager/secrets-store-csi-driver-provider-aws
   helm upgrade --install -n kube-system csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver --set syncSecret.enabled=true
-  helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.12.0 --set installCRDs=true
+  # helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.12.0 --set installCRDs=true
   # switch away from external secrets for now
   #  kubectl create namespace external-secrets
   #  kubectl apply -f charts/secret/templates/spc.yaml -n yellow-taxi
